94e89f788829fac4dcb40f0b6a999a11ef40a06d
javac0f <jcof28@gmail.com>
2023-11-11 18:25:25 -0500

    more UI Changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from taipy.gui import Gui
+from taipy.gui import Gui, Markdown
 import pages
 
 # TO ACTIVATE ENVIRONMENT: C:/Users/jcof2/anaconda3/Scripts/activate
@@ -16,7 +16,6 @@ if(__name__=='__main__'):
     Gui(pages = page_dict, 
         css_file='./STYLES/style.css').run( title="SouthPool",
                                             use_reloader=True,
-                                            stylekit=False,
-                                            port=69)
+                                            stylekit=False)
 
 
--- pages.py
+++ pages.py
@@ -2,22 +2,20 @@ import taipy as tp
 
 
 home_page = """
-# SOUTHPOOL
+<center>\n<|navbar|>\n</center>
+# <strong>SOUTH</strong>POOL
+
+
 
 <|25 75|layout|gap=30px|
 
 <|sidebar|
 ## Please **filter** here:
-
 <||selector||multiple|label=Select the City|dropdown||class_name=fullwidth|>
-
 <||selector||multiple|label=Select the Customer Type|dropdown||class_name=fullwidth|>
-
 <||selector||multiple|label=Select the Gender|dropdown||class_name=fullwidth|>
 |>
 
-
-<|1 1 1|layout|>
 
 
 |>
