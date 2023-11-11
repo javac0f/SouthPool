c54f306abf75231a661bbea92748019bfb42f22e
javac0f <jcof28@gmail.com>
2023-11-11 18:13:12 -0500

    Added UI stying choices

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+from taipy.gui import Gui
+import pages
+
+# TO ACTIVATE ENVIRONMENT: C:/Users/jcof2/anaconda3/Scripts/activate
+#                           conda activate base 
+
+
+page_dict = {
+    '/': pages.root_page,
+    'home':pages.home_page
+}
+
+
+if(__name__=='__main__'):
+    Gui(pages = page_dict, 
+        css_file='./STYLES/style.css').run( title="SouthPool",
+                                            use_reloader=True,
+                                            stylekit=False,
+                                            port=69)
+
+
---
+++ pages.py
@@ -0,0 +1,29 @@
+import taipy as tp
+
+
+home_page = """
+# SOUTHPOOL
+
+<|25 75|layout|gap=30px|
+
+<|sidebar|
+## Please **filter** here:
+
+<||selector||multiple|label=Select the City|dropdown||class_name=fullwidth|>
+
+<||selector||multiple|label=Select the Customer Type|dropdown||class_name=fullwidth|>
+
+<||selector||multiple|label=Select the Gender|dropdown||class_name=fullwidth|>
+|>
+
+
+<|1 1 1|layout|>
+
+
+|>
+
+"""
+
+
+root_page = """
+"""
